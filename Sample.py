0ad7c4bc7e105671d033552081a772cfc0477c77
Chatur <Tariq.Chatur@gov.bc.ca>
2022-11-03 14:29:24 -0700

    Create Sample.py

---
+++ Sample.py
@@ -0,0 +1,11 @@
+class Sample:
+   def __init__(self) -> None:
+      self.folderName  = ""
+      self.highestPoint      = [0,0,0]
+      
+
+   def calc_highest_point(self):
+      pass
+
+   def read_file_in_folder(self):
+      pass
